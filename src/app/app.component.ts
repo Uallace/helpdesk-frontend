46a4339524fa91b10507e54a2373863a3b1bfe28
uallace-gomes-silva <uallace.gomes.silva@ibm.com>
2023-07-28 11:28:51 -0300

    Adicionado imports necessarios do projeto  no app.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:`<router-outlet></router-outlet>`
+  template:`<button mat-raised-button color="primary">Primary</button>
+      <router-outlet></router-outlet>`
 })
 export class AppComponent {
   title = 'helpdesk-frontend';
