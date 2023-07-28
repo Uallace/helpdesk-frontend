03ce39c80c02f7ba36ba44a8a2303f17b732fbc8
uallace-gomes-silva <uallace.gomes.silva@ibm.com>
2023-07-28 16:18:10 -0300

    Componente home adicionado ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,12 +29,14 @@ import { MatCardModule } from '@angular/material/card';
 
 //components do projeto
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
