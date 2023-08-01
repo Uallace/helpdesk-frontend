b986c39006622da51f24a9d255774ef2e156156f
uallace-gomes-silva <uallace.gomes.silva@ibm.com>
2023-08-01 17:25:57 -0300

    Componente tecnico create validado e finalizado

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,13 +12,53 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class TecnicoCreateComponent {
 
+  tecnico: Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+
   nome: FormControl = new FormControl(null, Validators.minLength(3));
   cpf: FormControl = new FormControl(null, Validators.required);
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor(
+    private tecnicoService: TecnicoService,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
 
+  create(): void {
+    this.tecnicoService.create(this.tecnico).subscribe(() => {
+      this.toastr.success('Técnico cadastrado com sucesso!', 'Cadastro');
+      this.router.navigate(['tecnicos'])
+    }, ex => {
+      
+      if(ex.error.errors){
+        ex.error.errors.forEach(element => {
+          this.toastr.error(element.message);
+        });
+      } else {
+        this.toastr.error(ex.error.message);
+      }
+
+    });
+  }
+
+  addPerfil(perfil: any): void {
+
+    if(this.tecnico.perfis.includes(perfil)){
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    } else {
+      this.tecnico.perfis.push(perfil);
+    }
+  }
+  
   validaCampos() {
     return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid;
   }
